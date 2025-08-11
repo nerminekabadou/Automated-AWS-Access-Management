bf8d3799df57b524f98851e74a0988e75da725be
nerminekabadou <kabadounermine@gmail.com>
2025-08-11 12:01:28 +0100

    Fixed bugs

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 #Backend config implementation to track state of the infrastructure deployed on the AWS account
 terraform {
   backend "s3" {
-    bucket         = "our-terraform-state-bucket"
+    bucket         = "our-terraform-state-bucket-2025"
     key            = "internshipproject/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "terraform-locks"
+    use_lockfile  = true  # instead of dynamodb_table = "terraform-locks"  (deprecated)
     encrypt        = true
   }
 }
