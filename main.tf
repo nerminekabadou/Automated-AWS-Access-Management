47b6637d4cd6dd8e7ab3a02ad11c5f4f65ef065c
nerminekabadou <kabadounermine@gmail.com>
2025-08-13 16:22:10 +0100

    Lambdas Test were Successful

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "lambda" {
   region          = var.region
   account_id      = var.account_id
   api_gateway_url = module.api_gateway.api_url
-  api_gateway_id  = module.api_gateway.api_id
 }
 
 module "api_gateway" {
